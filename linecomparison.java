eb7bd493b9c9be1ac1cc26a7e6d7c39e1f209b65
omkarmali4437 <omkarmalli688@gmail.com>
2021-02-21 20:30:46 +0530

    Added logic for calculating length of line

--- linecomparison.java
+++ linecomparison.java
@@ -1,5 +1,19 @@
-public class empwagebuilder {
+import java.util.Scanner;
+
+public class linecomparison {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Line Comparison Problem");
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter x1 co-ordinate: ");
+		int x1=scan.nextInt();
+		System.out.println("Enter y1 co-ordinate: ");
+      int y1=scan.nextInt();
+		System.out.println("Enter x2 co-ordinate: ");
+      int x2=scan.nextInt();
+		System.out.println("Enter y2 co-ordinate: ");
+      int y2=scan.nextInt();
+		scan.close();
+		int length=(int)Math.sqrt((Math.pow(x2-x1,2)) + (Math.pow(y2-y1,2)));
+		System.out.println("Length of line is: "+length);
 	}
 }
